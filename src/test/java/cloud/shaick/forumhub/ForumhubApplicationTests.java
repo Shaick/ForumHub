c1f7324baca4ab4c1ac0c9b4d3fb033df7764af4
Wilson Cardoso (shaick) <shaick@msn.com>
2024-07-15 23:59:11 -0300

    inicio

---
+++ src/test/java/cloud/shaick/forumhub/ForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.shaick.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
